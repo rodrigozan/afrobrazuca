eceec0cc65c1ff276ea1abd9b3a40a143099652b
Rodrigo Zandonadi <rodzandonadi@gmail.com>
2024-06-17 11:01:20 -0300

    Import components in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
+import React from 'react';
+import Header from '../components/Header';
+import About from '../components/About';
+import Musics from '../components/Musics';
+import Contact from '../components/Contact';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <main>
-      oi
+    <main className="container">
+      <Header />
+      <About />
+      <Musics />
+      <Contact />
     </main>
   );
 }
