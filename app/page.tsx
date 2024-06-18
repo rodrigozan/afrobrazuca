c65ccb631beb9a26f2528c2afc9344c52aa6b2df
Rodrigo Zandonadi <rodzandonadi@gmail.com>
2024-06-18 15:07:16 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
-import Header from '../components/Header';
-import About from '../components/About';
-import Musics from '../components/Musics';
-import Contact from '../components/Contact';
+import About from './components/About';
+import Musics from './components/Musics';
+import Contact from './components/Contact';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+//import 'bootstrap/dist/css/bootstrap.min.css';
+import './globals.css'
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className="container">
-      <Header />
       <About />
       <Musics />
       <Contact />
