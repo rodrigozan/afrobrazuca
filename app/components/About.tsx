951454475a6ce6cf02f0dd564184ac2bb7e94c85
Rodrigo Zandonadi <rodzandonadi@gmail.com>
2024-06-18 15:00:37 -0300

    Create About.tsx

---
+++ app/components/About.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import styles from '../../styles/About.module.scss';
+
+import about from '../assets/img/afrobrazuca-about.jpg'
+
+const About: React.FC = () => {
+  return (
+    <section className={`${styles.About} p-4 row`}>
+      <div className='col-6'>
+        <img src={about.src} alt="About Afrobrazuca" />
+      </div>
+      <div className="col-6">
+        <h5>Afrobrazuca</h5>
+        <p>
+          Afrobrazuca is a talented artist and music producer specializing in lofi worship, blending lo-fi beats with relaxing and Christian melodies. His compositions are an invitation to tranquility and reflection, providing a unique sonic experience that uplifts the soul and calms the spirit. With an impressive ability to blend traditional lo-fi elements with spiritual nuances, Afrobrazuca creates tracks that touch the heart and inspire devotion.
+        </p>
+        <p>
+          His work is a true musical journey that celebrates faith and serenity, standing out in the contemporary Christian music scene. Each produced track is a work of art that combines the modernity of lo-fi with the depth of religious music, making Afrobrazuca an indispensable name for genre lovers.
+        </p>
+      </div>
+
+    </section>
+  );
+};
+
+export default About;
