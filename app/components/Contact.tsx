4cf5924b73c26c595742dbe2578959f2d7f3e1e3
Rodrigo Zandonadi <rodzandonadi@gmail.com>
2024-06-18 15:00:42 -0300

    Create Contact.tsx

---
+++ app/components/Contact.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Contact: React.FC = () => {
+  return (
+    <section className="p-4">
+      <h2>Contato</h2>
+      <p>
+        Entre em contato pelo e-mail: artista@afrobrazuca.com
+      </p>
+    </section>
+  );
+};
+
+export default Contact;
