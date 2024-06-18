8ef7b2a01bd8558e10ada644c468a1e23e78d34d
Rodrigo Zandonadi <rodzandonadi@gmail.com>
2024-06-18 15:05:54 -0300

    Update next.config.mjs
    Configure Sass in Nextjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+
+export function getRootDirectoryPath() {
+    const __filename = fileURLToPath(import.meta.url);
+    return dirname(__filename);
+  }
+
+  const rootDirectoryPath = getRootDirectoryPath(); 
+
+  const nextConfig = {
+    sassOptions: {
+      includePaths: [path.join(rootDirectoryPath, 'styles')],
+    },
+  };
 
 export default nextConfig;
