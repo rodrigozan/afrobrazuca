49585a17b8e79ef8c54261515cf3f6f020a54bbe
Rodrigo Zandonadi <rodzandonadi@gmail.com>
2024-06-18 15:00:47 -0300

    Create Header.tsx

---
+++ app/components/Header.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import styles from '../../styles/Header.module.scss';
+
+import spotify from '../assets/img/spotify-white.png'
+import apple from '../assets/img/apple-music-white.png'
+
+const Header: React.FC = () => {
+  return (
+    <header className={styles.header}> 
+      <h1 className='text-primary'>Afrobrazuca</h1>
+      <p>
+        <a href="https://open.spotify.com/intl-pt/artist/2vQYZkXsLPj0QALQaSINWr" className="btn btn-success m-2 text-white"> <img src={spotify.src} /> Ouça no Spotify</a>
+        <a href="https://apple.com/music" className="btn btn-light m-2 text-white"><img src={apple.src} /> Ouça no Apple Music</a>
+      </p>
+    </header>
+  );
+};
+
+export default Header;
