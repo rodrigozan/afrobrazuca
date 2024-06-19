261c0c8c9492f199e94af29479e09378230856b7
Rodrigo Zandonadi <rodzandonadi@gmail.com>
2024-06-19 09:32:45 -0300

    Updated Musics component andMusics module scss

--- app/components/Musics.tsx
+++ app/components/Musics.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
 
+import styles from '../../styles/Musics.module.scss';
+
+import img1 from '../assets/img/RodLofi-about.jpg'
+import img2 from '../assets/img/RodLofi-header.jpg'
+
 const Musics: React.FC = () => {
   return (
-    <section className="p-4 bg-light">
-      <h2>Músicas</h2>
-      <ul>
-        <li>Musica 1</li>
-        <li>Musica 2</li>
-        <li>Musica 3</li>
-      </ul>
+    <section className={`${styles.Musics} container p-4 bg-light`}>
+      <h5 className='text-info'>Musics</h5>
+      <div className='d-flex'>
+        <figure className={`${styles.music} music1 col-6 p-5`}>
+          <img src={img1.src} alt="Elephant at sunset" />
+          <figcaption>An elephant at sunset</figcaption>
+        </figure>
+        <figure className={`${styles.music} music1 col-6 p-5`}>
+          <img src={img2.src} alt="Elephant at sunset" />
+          <figcaption>An elephant at sunset</figcaption>
+        </figure>
+      </div>
     </section>
   );
 };
