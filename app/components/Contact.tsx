bc947acf20d611aaaf3fba1577e6c207beb726a3
Rodrigo Zandonadi <rodzandonadi@gmail.com>
2024-07-01 12:04:45 -0300

    Update Contact.tsx

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -1,12 +1,31 @@
 import React from 'react';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+
+import styles from '../../styles/Contact.module.scss'
+
 const Contact: React.FC = () => {
   return (
-    <section className="p-4">
-      <h2>Contato</h2>
-      <p>
-        Entre em contato pelo e-mail: <a href="mailto:rodlofi@gmail.com" className='text-dark'>rodlofi@gmail.com</a>
-      </p>
+    <section className={`${styles.Contact} p-4 bg-dark`}>
+      <div className={`content`}>
+        <p>Me in web</p>
+        <div className="socialMedia d-flex align-items-center">
+          <a href="mailto:rodlofi@gmail.com" className='text-white w-25 me-2'>
+            <FontAwesomeIcon icon={fas.faEnvelope} />
+          </a>
+          <a href="https://open.spotify.com/intl-pt/artist/2vQYZkXsLPj0QALQaSINWr" className="text-white w-25 me-2">
+            <FontAwesomeIcon icon={fab.faSpotify} /></a>
+            <a href="https://www.youtube.com/@rodlofi" className="text-white w-25 me-2">
+            <FontAwesomeIcon icon={fab.faYoutube} /></a>
+            <a href="https://www.instagram.com/rodlofi" className="text-white w-25 me-2">
+            <FontAwesomeIcon icon={fab.faInstagram} /></a>
+            <a href="https://www.tiktok.com/@rod.lofi" className="text-white w-25">
+            <FontAwesomeIcon icon={fab.faTiktok} /></a>
+        </div>
+      </div>
     </section>
   );
 };
