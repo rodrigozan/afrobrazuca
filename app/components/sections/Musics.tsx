0e8d3ee3db0f7af7da4f50f5e561d6e9d9a0c152
Rodrigo Zandonadi <rodzandonadi@gmail.com>
2024-07-01 14:42:32 -0300

    Update Musics.tsx

--- app/components/sections/Musics.tsx
+++ app/components/sections/Musics.tsx
@@ -1,22 +1,35 @@
-import React from 'react';
+"use client";
+
+import React, { useState } from 'react';
+
+import Modal from '../utilities/Modal';
+import MusicPlayer from '../utilities/MusicPlayer';
 
 import styles from '../../../styles/modules/Musics.module.scss';
 
-import img1 from '../../assets/img/RodLofi-about.jpg'
-import img2 from '../../assets/img/RodLofi-header.jpg'
+import img1 from '../../assets/img/waves.jpg'
+import img2 from '../../assets/img/one-day-in-your-presence.jpg'
 
 const Musics: React.FC = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
+
   return (
     <section className={`${styles.Musics} container p-4 bg-light`}>
       <h5 className='text-info'>Musics</h5>
       <div className='d-flex'>
-        <figure className={`${styles.music} music1 col-6 p-5`}>
-          <img src={img1.src} alt="Elephant at sunset" />
-          <figcaption>An elephant at sunset</figcaption>
+        <figure className={`${styles.music} music1 col-6`}>
+          <img src={img1.src} alt="Waves" />
+          <figcaption className="d-flex align-items-end" onClick={openModal}>Waves</figcaption>
+          <Modal isOpen={isModalOpen} onClose={closeModal}>
+            <MusicPlayer src="../../assets/audio/waves.mp3" />
+          </Modal>
         </figure>
-        <figure className={`${styles.music} music1 col-6 p-5`}>
-          <img src={img2.src} alt="Elephant at sunset" />
-          <figcaption>An elephant at sunset</figcaption>
+        <figure className={`${styles.music} music1 col-6`}>
+          <img src={img2.src} alt="One day in your presence" />
+          <figcaption className="d-flex align-items-end">One day in your presence</figcaption>
         </figure>
       </div>
     </section>
