c24de300751c4c08ea1abf06efce0c1f165fbf1c
Rodrigo Zandonadi <rodzandonadi@gmail.com>
2024-07-01 12:04:07 -0300

    Create Footer.tsx

---
+++ app/components/Footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import FormattedDate from './FormattedDate';
+
+
+export default function Footer() {
+    const date = new Date();
+    
+    return (
+        <footer className='text-center text-white bg-black p-4'>
+            <p>© Rod Lofi, <FormattedDate date={date} format="Year" /></p>
+        </footer>
+    );
+}
