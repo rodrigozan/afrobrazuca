60aa686796ae6d58e06a70ff6fb712041f5a74bc
Rodrigo Zandonadi <rodzandonadi@gmail.com>
2024-07-03 08:57:48 -0300

    Update Modal.tsx

--- app/components/utilities/Modal.tsx
+++ app/components/utilities/Modal.tsx
@@ -1,4 +1,6 @@
 import React, { ReactNode } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { fas } from '@fortawesome/free-solid-svg-icons'
 
 import styles from '../../../styles/components/Modal.module.scss'
 
@@ -6,18 +8,18 @@ interface ModalProps {
   isOpen: boolean;
   onClose: () => void;
   children: ReactNode;
+  fadeType: 'fade-in' | 'fade-out';
 }
 
-const Modal: React.FC<ModalProps> = ({ isOpen, onClose, children }) => {
+const Modal: React.FC<ModalProps> = ({ isOpen, onClose, fadeType, children }) => {
   if (!isOpen) return null;
-
   return (
-    <div className={styles.modalBackground} onClick={onClose}>
-      <div className={styles.modalContent} onClick={e => e.stopPropagation()}>
-        <button className="btn btn-danger" onClick={onClose}>
-          &times;
-        </button>
+    <div className={`${styles.modalBackground} ${isOpen ? fadeType : ''}`} onClick={onClose}>
+      <div className={styles.modalContent} onClick={(e) => e.stopPropagation()}>
         {children}
+        <button className="btn btn-danger" onClick={onClose}>
+          <FontAwesomeIcon icon={fas.faXmark} />
+        </button>
       </div>
     </div>
   );
